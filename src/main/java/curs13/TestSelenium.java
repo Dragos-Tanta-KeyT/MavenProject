ce3c075dfca6a9122a0c9b30450fbba34c246eab
Dragos Tanta <dragos.tanta@gmail.com>
2023-10-24 19:25:57 +0300

    commit initial

---
+++ src/main/java/curs13/TestSelenium.java
@@ -0,0 +1,40 @@
+package curs13;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium {
+	
+	@Test
+	public void openBrowser() {
+		
+		//WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new FirefoxDriver();
+		//System.setProperty("webdriver.chrome.driver", "path catre chromedriver.exe");	
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://keybooks.ro");
+		
+		//<a href="https://keybooks.ro/contacts/">Contacts</a>
+		driver.findElement(By.linkText("CONTACTS")).click();
+	
+		/*
+		 * tagname
+		 * linkText
+		 * partialLinkText
+		 * className
+		 * id
+		 * name
+		 * css selector
+		 * xpath
+		 * 
+		 */
+		
+		
+	}
+	
+
+}
